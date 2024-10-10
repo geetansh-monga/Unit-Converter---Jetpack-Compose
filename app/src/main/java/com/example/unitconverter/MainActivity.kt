13c2cc38b591cf1d64718d3d75f19f230fe86a2a
Geetansh Monga <gmgog0402@gmail.com>
2024-10-10 19:51:41 +0530

    Use of Remember and dropdown expansion working added.

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,21 +5,38 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,22 +54,78 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column {
-        Text(text = "Unit Converter:")
-        OutlinedTextField(value = "", onValueChange = {} )
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    val conversionFactor = remember {
+        mutableDoubleStateOf(0.01)
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+    ) {
+        Text(text = "Padding:")
+        Text(text = "Unit Converter:", modifier = Modifier.padding(16.dp))
+
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {inputValue = it},
+            label = { Text(text = "Enter Value")}
+            
+        )
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             val context = LocalContext.current
-            Button(onClick = { Toast.makeText(context, "Thank you for clicking me!", Toast.LENGTH_SHORT).show() })
-            {
-                Text("Select")
-                Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+            //Input Box
+            Box {
+                //Input Button
+                Button(onClick = {
+                    iExpanded = true
+                    Toast.makeText(
+                        context,
+                        "Thank you for clicking me!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+                {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = iExpanded, onDismissRequest = { }){
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = {  iExpanded=false })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = {  iExpanded=false })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = {  iExpanded=false })
+                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = {  iExpanded=false })
+                }
             }
-            Button(onClick = { Toast.makeText(context, "Thank you for clicking me!", Toast.LENGTH_SHORT).show() })
-            {
-                Text("Select")
-                Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+            Spacer(modifier = Modifier.width(10.dp))
+            Box {
+                Button(onClick = {
+                    Toast.makeText(
+                        context,
+                        "Thank you for clicking me!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                })
+                {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "")
+                }
+                DropdownMenu(expanded = oExpanded, onDismissRequest = { /*TODO*/ }){
+                    DropdownMenuItem(text = { Text("Centimeters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Meters") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Feet") }, onClick = { /*TODO*/ })
+                    DropdownMenuItem(text = { Text("Millimeters") }, onClick = { /*TODO*/ })}
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
         Text(text = "Result:")
     }
 }
